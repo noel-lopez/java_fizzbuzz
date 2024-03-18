ad3e515ce82fab0bebc41911090c279afe59226c
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 14:00:00 +0100

    feat: convert 15 into FizzBuzz

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
+        if (number == 15) {
+            return "FizzBuzz";
+        }
         if(number % 5 == 0) {
             return "Buzz";
         }
