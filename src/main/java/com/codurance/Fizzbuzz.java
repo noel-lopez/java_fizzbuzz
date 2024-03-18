7d9bbc00de7dfaab9d853d219befa95a67bfa091
Paula Masutier <paulamasutier@gmail.com>
2024-03-18 14:02:09 +0100

    Feat: convert 30 into FizzBuzz

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,7 +3,7 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
-        if (number == 15) {
+        if (number == 15 || number == 30) {
             return "FizzBuzz";
         }
         if(number % 5 == 0) {
