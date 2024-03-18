393ce5772c6616ad43c2beac6b0eefd5fef1a75f
Paula Masutier <paulamasutier@gmail.com>
2024-03-18 13:54:14 +0100

    Feat: convert 20 into Buzz

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,7 +3,7 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
-        if(number == 5 || number == 10) {
+        if(number == 5 || number == 10 || number == 20) {
             return "Buzz";
         }
         if (number % 3 == 0) {
