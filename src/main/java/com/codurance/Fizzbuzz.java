ad38390c61842a34f05835bc62c175a204865817
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 13:32:18 +0100

    feat: convert 3 into Fizz

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
+        if (number == 3) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
