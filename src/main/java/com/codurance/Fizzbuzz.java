8835ae3074d98050b53e1ee2f61b2e7d60420175
Paula Masutier <paulamasutier@gmail.com>
2024-03-18 13:15:28 +0100

    Refactor: iteration for base behaviour for non changing numbers

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,12 +3,6 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
-        if(number == 4) {
-            return "4";
-        }
-        if (number == 2) {
-            return "2";
-        }
-        return "1";
+        return String.valueOf(number);
     }
 }
