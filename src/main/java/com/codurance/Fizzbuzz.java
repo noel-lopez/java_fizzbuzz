32098eb554547f94b5ea71d313ec02f790ec60f4
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 13:37:32 +0100

    feat: convert 9 into Fizz

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,7 +3,7 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
-        if (number == 3 || number == 6) {
+        if (number == 3 || number == 6 || number == 9) {
             return "Fizz";
         }
         return String.valueOf(number);
