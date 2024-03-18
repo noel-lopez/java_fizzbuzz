eb57b19d9aa5b156c415f2f5a484cd2e70bef52c
Noel Lopez <nlopez.publi@gmail.com>
2024-03-18 13:05:42 +0100

    feat: convert 2 to 2

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
+        if (number == 2) {
+            return "2";
+        }
         return "1";
     }
 }
