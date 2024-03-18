b401d1a4f25c1bb6ce71670bd585b56095715929
Paula Masutier <paulamasutier@gmail.com>
2024-03-18 13:08:40 +0100

    Feat: convert 4 to 4

--- src/main/java/com/codurance/Fizzbuzz.java
+++ src/main/java/com/codurance/Fizzbuzz.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class Fizzbuzz {
 
     public static String convert(int number) {
+        if(number == 4) {
+            return "4";
+        }
         if (number == 2) {
             return "2";
         }
